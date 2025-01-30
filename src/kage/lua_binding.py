4bfbea5688e65b888037134a5ce59c166bf99c1b
kurogedelic <kurogedelic@gmail.com>
2025-01-30 14:02:01 +0900

    fix kage

--- src/kage/lua_binding.py
+++ src/kage/lua_binding.py
@@ -1,10 +1,24 @@
 # src/kage/lua_binding.py
+from lupa import LuaRuntime
+import time
+
+
 class KageLuaEngine:
+    def __init__(self, kage_instance):
+        self.kage = kage_instance
+        self.lua = LuaRuntime(unpack_returned_tuples=True)
+        self.frame_interval = 1.0 / 30
+        self.last_frame_time = time.time()
+        self.setup_api()
+
     def setup_api(self):
         g = self.lua.globals()
 
         # トップレベルAPI
         g.print = print
+
+        # math
+        g.math = self.lua.eval('math')
 
         # Kage APIテーブル
         kage_api = {
@@ -42,3 +56,26 @@ class KageLuaEngine:
             'setLineWidth': self.kage.set_line_width,
         }
         g.kage = kage_api
+
+    def load_script(self, script_path):
+        with open(script_path, 'r') as f:
+            self.lua.execute(f.read())
+
+    def run(self):
+        if 'init' in self.lua.globals():
+            self.lua.globals().init()
+
+        try:
+            while True:
+                current_time = time.time()
+
+                if 'update' in self.lua.globals():
+                    self.lua.globals().update()
+
+                elapsed = time.time() - current_time
+                wait_time = self.frame_interval - elapsed
+                if wait_time > 0:
+                    time.sleep(wait_time)
+
+        except KeyboardInterrupt:
+            print("\nScript terminated by user")
